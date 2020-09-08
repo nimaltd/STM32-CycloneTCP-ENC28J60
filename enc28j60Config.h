e7065b03d780caadd90cf16848a06b5d502550c3
Nima Askari ----- نیما عسکری <nimaltd@yahoo.com>
2020-09-08 11:58:14 +0430

    Update enc28j60Config.h

--- enc28j60Config.h
+++ enc28j60Config.h
@@ -8,10 +8,10 @@
   Youtube:    https://www.youtube.com/channel/UCUhY7qY1klJm1d2kulr9ckw  
 */
 
-#define		_ENC28J60_EXT			EXTI4_IRQn
-#define		_ENC28J60_EXT_PIN		GPIO_PIN_4
-#define		_ENC28J60_CS_GPIO		SPI1_CS_GPIO_Port	
-#define		_ENC28J60_CS_PIN		SPI1_CS_Pin
-#define		_ENC28J60_SPI			hspi1
+#define   _ENC28J60_EXT       EXTI4_IRQn
+#define   _ENC28J60_EXT_PIN   GPIO_PIN_4
+#define   _ENC28J60_CS_GPIO   SPI1_CS_GPIO_Port	
+#define   _ENC28J60_CS_PIN    SPI1_CS_Pin
+#define   _ENC28J60_SPI       hspi1
 
 #endif
